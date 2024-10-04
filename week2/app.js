bcb765e0713ded1c8d051a7e269a12db0848b241
Yipmong Said <Syipmong5@gmail.com>
2024-10-04 16:49:28 +0100

    Refactor server logic to handle different request types and URLs

--- week2/app.js
+++ week2/app.js
@@ -1,27 +1,36 @@
-import { createServer } from 'http';
+import { createServer } from "http";
 
-const server = createServer((req, res)=>{
-    if(req.method === 'GET' && req.url === '/json'){
-        res.writeHead(200, {'Content-Type': 'text/plain'});
-        const jsonResponse = {
-            message: 'Hello, json',
-            status: 200
-        }
-        res.end(
-            JSON.stringify(jsonResponse)
-        )
-    }else if(req.method === 'GET' && req.url === '/html'){
-        res.writeHead(200, {'Content-Type': 'text/html'});
-        res.end('<h1>Hello, HTML</h1>');
-    }else if(req.method === 'POST'){
-        res.writeHead(200,{'content-type': 'test/plain'});
-        res.end('This is a POST request');
-    }else{
-        res.writeHead(404, {'Content-Type': 'text/plain'});
-        res.end('404 Not Found');
-    }
-})
+const server = createServer((req, res) => {
+  console.log(`Received request method: ${req.method}`);
+  console.log(`Request URL: ${req.url}`);
+  console.log(`Request Headers:`, req.headers);
 
-server.listen(3000, ()=>{
-    console.log('Server is running on port 3000');
-})
+  res.writeHead(200, { "Content-Type": "text/plain" });
+  res.end("Check your terminal for request details!\n");
+  if (!res.headersSent) {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end('Hello, world!');
+  }
+
+  else if (req.method === "GET" && req.url === "/json") {
+    res.writeHead(200, { "Content-Type": "text/plain" });
+    const jsonResponse = {
+      message: "Hello, json",
+      status: 200,
+    };
+    res.end(JSON.stringify(jsonResponse));
+  } else if (req.method === "GET" && req.url === "/html") {
+    res.writeHead(200, { "Content-Type": "text/html" });
+    res.end("<h1>Hello, HTML</h1>");
+  } else if (req.method === "POST") {
+    res.writeHead(200, { "content-type": "test/plain" });
+    res.end("This is a POST request");
+  } else {
+    res.writeHead(404, { "Content-Type": "text/plain" });
+    res.end("404 Not Found");
+  }
+});
+
+server.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
