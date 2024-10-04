53fb5ce71e765b5cf01a884d38d45554b4de0b3e
Yipmong Said <Syipmong5@gmail.com>
2024-10-04 15:47:30 +0100

    adding week2 works

--- week2/app.js
+++ week2/app.js
@@ -1,15 +1,16 @@
-const http = require('http')
+import { createServer } from 'http';
 
-const server = http.createServer(
+const server = createServer(
     (req,res)=>{
+        
         res.writeHead(200,{
-            'content-type': 'text/plain'
+            'content-type': 'text/html'
         });
-        res.end('Hello World');
+        res.end('Hello World\n I am a great person');
 })
 
 
 server.listen(3000,()=>{
-    console.log("listening on port 3000...")
+    console.log("Server running at http://localhost:3000/")
 }
 )
