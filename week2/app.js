b4030169b8f2b508fa9be4b5fbd3c5b48a9f1a48
Yipmong Said <Syipmong5@gmail.com>
2024-10-04 15:58:36 +0100

    adding week2 works

--- week2/app.js
+++ week2/app.js
@@ -1,16 +1,18 @@
 import { createServer } from 'http';
 
-const server = createServer(
-    (req,res)=>{
-        
-        res.writeHead(200,{
-            'content-type': 'text/html'
-        });
-        res.end('Hello World\n I am a great person');
+const server = createServer((req, res)=>{
+    if(req.method === 'GET'){
+        res.writeHead(200, {'Content-Type': 'text/plain'});
+        res.end('This is a Get request')
+    } else if(req.method === 'POST'){
+        res.writeHead(200,{'content-type': 'test/plain'});
+        res.end('This is a POST request');
+    }else{
+        res.writeHead(404, {'Content-Type': 'text/plain'});
+        res.end('404 Not Found');
+    }
 })
 
-
-server.listen(3000,()=>{
-    console.log("Server running at http://localhost:3000/")
-}
-)
+server.listen(3000, ()=>{
+    console.log('Server is running on port 3000');
+})
