9090c7c8424d1d6d1356e44524f010c3f49cc13a
Yipmong Said <Syipmong5@gmail.com>
2024-10-04 17:10:04 +0100

    Refactor server logic to handle different request types and URLs

--- week2/app.js
+++ week2/app.js
@@ -1,12 +1,36 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-  console.log(`Received request method: ${req.method}`);
-  console.log(`Request URL: ${req.url}`);
-  console.log(`Request Headers:`, req.headers);
+  if(req.url === '/'){
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.end(
+        '<h1>Hello, World!</h1>' +
+        '<p>Welcome to the home page.</p>'
+    );
+  }else if(req.url === '/contact'){
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.end(
+        '<h1>Contact</h1>' +
+        '<p>Ways to contact us</p>' +
+        '<ul>' +
+        '<li>Phone: 123-456-7890</li>' +
+        '<li>Email: <a href="mailto:info@example.com">info@example.com</a></li>' +
+        '</ul>'
+    );
 
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Check your terminal for request details!\n');
+  }else if(req.url === '/about'){
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.end(
+        '<h1>About</h1>' +
+        '<p>Learn more about us.</p>'
+    );
+  }else{
+    res.writeHead(404, {'Content-Type': 'text/html'});
+    res.end(
+        '<h1>404 Not Found</h1>' +
+        '<p>Sorry, the page you are looking for cannot be found.</p>'
+    );
+  }
 });
 
 server.listen(3000, () => {
