b8dfb10758c4a1bdb1faff73a227b50bf656cf47
Yipmong Said <Syipmong5@gmail.com>
2024-10-05 16:42:12 +0100

    install the express server module for an efficient workflow

--- week3/app.js
+++ week3/app.js
@@ -1,11 +1,34 @@
-const express = require('express')
-const path = require('path')
-const app = express()
-const port = 3000
+const express = require('express');
+const app = express();
+const port = 3000;
+
+
+app.use(
+    '/',
+    (req,res,next)=>{
+        console.log(
+            `${req.method} Method on ${req.url}`
+        )
+        next();
+    }
+)
 
 app.get(
-    '/', (req, res) => res.sendFile(
-        path.join(__dirname, './index.html')
-    )
+    '/',
+    (req, res) =>{
+        res.send('Hello World');
+    }
+);
+
+app.get(
+    '/about',
+    (req,res) =>{
+        res.send('About Us');
+    }
+);
+
+
+app.listen(
+    port,
+    () => console.log(`Listening on port ${port}...`)
 )
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
