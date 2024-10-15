93dd92e9278f0632fbd4f714cdd5b97ff2ea53fc
Yipmong Said <Syipmong5@gmail.com>
2024-10-15 11:21:57 +0100

    Update package.json and app.js for week 4 projects

--- week4/app.js
+++ week4/app.js
@@ -3,32 +3,26 @@ const app = express();
 const bodyParser = require('body-parser')
 const port = 3000
 
+
+let users = [
+    {id: 1, name: 'Alice', email: 'alice@gmail.com'},
+    {id: 2, name: 'Bob', email: 'bob@gmail.com'},
+    {id: 3, name: 'Charlie', email: 'charlie@gmail.com'},
+    {id: 4, name: 'David', email: 'david@gmail.com'},
+    {id: 5, name: 'Eve', email: 'eve@gmail.com'}
+]
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended:true}))
 
-app.get('/',
-    (req,res) =>{
-        res.send("Welcome to the home page")
-    }
-)
-app.get(
-    '/contact',
-    (req,res) =>{
-        res.send("Contact Us");
-    
-    }
-    
-)
 
-// error handling
-app.use((req, res, next) => {
-  res.status(404).send("Sorry can't find that!")
+app.get('/api/users',(req,res)=>{
+    res.json(users)
 })
 
 
-app.listen(port,
-    () =>{
-        console.log(`Listening on Port ${port}...`)
 
+app.listen(
+    port,()=>{
+        console.log(`Server is running on http://localhost:${port}`)
     }
 )
