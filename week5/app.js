f1ed4c84c49ad56d99e5f0f3cf77b104784043a2
Yipmong Said <Syipmong5@gmail.com>
2024-10-15 14:57:30 +0100

    Refactor app.js to add user creation endpoint

--- week5/app.js
+++ week5/app.js
@@ -14,12 +14,33 @@ mongoose.connect(
 ).then(()=> console.log('Connected to MongoDb')).catch(err => console.log('could not connect to mongoDb ...', err))
 
 
+app.use(express.json())
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
+
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(dir, 'index.html'))
 })
+
+app.post(
+    '/api/users', async (req,res) =>{
+
+        try{
+            let user = new User({
+                name: req.body.name,
+                email: req.body.email,
+                age: req.body.age
+            });
+            user = await user.save();
+            res.send(user)
+        }
+        catch(err){
+            res.status(400).send(err.message)
+        }
+
+    }
+)
 
 app.listen(port, () => {
     console.log(`Server running on http://localhost:${port}`)
