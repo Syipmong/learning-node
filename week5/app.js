9d74bd94302ef0155ec8cdb3a5473466beaa343f
Yipmong Said <Syipmong5@gmail.com>
2024-10-15 15:03:07 +0100

    Refactor app.js to add user creation endpoint

--- week5/app.js
+++ week5/app.js
@@ -1,48 +1,42 @@
-const express = require('express')
-const app = express()
-const bodyParser = require('body-parser')
-const path = require('path')
-const port = 3000
-const dir = 'C:/NXDI/learning-node/pages'
-const mongoose = require('mongoose')
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+const mongoose = require('mongoose');
+const User = require('../models/user.js');
 
-
-const User = ('../models/user.js')
+const app = express();
+const port = 3000;
+const dir = 'C:/NXDI/learning-node/pages';
 
 mongoose.connect(
     'mongodb://localhost/mydatabase', {}
-).then(()=> console.log('Connected to MongoDb')).catch(err => console.log('could not connect to mongoDb ...', err))
+).then(()=> console.log('Connected to MongoDb')).catch(err => console.log('could not connect to mongoDb ...', err));
 
+app.use(express.json());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use(express.json())
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: true }))
-
-
-app.get('/', (req, res) => {
-    res.sendFile(path.join(dir, 'index.html'))
-})
+app.get('/', (_req, res) => {
+    res.sendFile(path.join(dir, 'index.html'));
+});
 
 app.post(
-    '/api/users', async (req,res) =>{
-
-        try{
+    '/api/users', async (req, res) => {
+        try {
             let user = new User({
                 name: req.body.name,
                 email: req.body.email,
                 age: req.body.age
             });
             user = await user.save();
-            res.send(user)
+            res.send(user);
+        } catch (err) {
+            res.status(400).send(err.message);
         }
-        catch(err){
-            res.status(400).send(err.message)
-        }
-
     }
-)
+);
 
 app.listen(port, () => {
-    console.log(`Server running on http://localhost:${port}`)
-})
+    console.log(`Server running on http://localhost:${port}`);
+});
 
