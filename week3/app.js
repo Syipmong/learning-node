3e2b7bd2429d5cb5cab271def07dd66a1c3023c8
Yipmong Said <Syipmong5@gmail.com>
2024-10-15 10:33:05 +0100

    Update package.json and app.js for week 3 projects

--- week3/app.js
+++ week3/app.js
@@ -6,12 +6,17 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-// Handle GET request
-app.get('/', (req, res) => {
-  res.send('Submit your data using POST to /submit');
+// Middleware to log request method and URL
+app.use((req, res, next) => {
+  console.log(`Request method: ${req.method}, Request URL: ${req.url}`);
+  next();
 });
 
-// Handle POST request
+// Handle GET request
+app.get('/', (req, res) => {
+  res.send('Submit your data using POST to /submit okay');
+});
+
 app.post('/submit', (req, res) => {
   const { name, email } = req.body;
   res.send(`Received data: Name - ${name}, Email - ${email}`);
