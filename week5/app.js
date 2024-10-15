50b0f19eeead950c4cf77247135811277fabbe03
Yipmong Said <Syipmong5@gmail.com>
2024-10-15 14:43:09 +0100

    Add missing dependencies and update package.json

--- week5/app.js
+++ week5/app.js
@@ -1,15 +1,27 @@
 const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
+const path = require('path')
 const port = 3000
+const dir = 'C:/NXDI/learning-node/pages'
+const mongoose = require('mongoose')
+
+
+const User = ('../models/user.js')
+
+mongoose.connect(
+    'mongodb://localhost/mydatabase', {}
+).then(()=> console.log('Connected to MongoDb')).catch(err => console.log('could not connect to mongoDb ...', err))
+
 
 app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({extended:true}))
+app.use(bodyParser.urlencoded({ extended: true }))
 
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/index.html')
+    res.sendFile(path.join(dir, 'index.html'))
 })
 
 app.listen(port, () => {
     console.log(`Server running on http://localhost:${port}`)
 })
+
